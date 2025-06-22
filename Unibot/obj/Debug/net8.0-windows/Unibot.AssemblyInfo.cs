7151812e24b00f839438d70ee4becdf70d9c70ef
pcstyle <pcstyle@duck.com>
2025-06-22 06:20:37 +0200

    Add initial configuration files for Unibot, including environment settings, launch configurations, and build tasks for .NET Core. Update project.assets.json and project.nuget.cache for local paths and dependencies.

--- Unibot/obj/Debug/net8.0-windows/Unibot.AssemblyInfo.cs
+++ Unibot/obj/Debug/net8.0-windows/Unibot.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyCopyrightAttribute("Copyright © 2025")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b3dea051e176bb85f135194dbc5fb5cad10b68d0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2442f1494701957037500141f822044562e89e19")]
 [assembly: System.Reflection.AssemblyProductAttribute("Unibot")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Unibot")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
